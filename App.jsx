244d3eb5125158243cc0d550400cddda36b58db9
boboqulovsherdiyor0102-spec <boboqulovsherdiyor0102@gmail.com>
2026-03-19 12:39:29 +0500

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1>Paradise Nursery</h1>
+        <p>Welcome to our online store for the best houseplants!</p>
+        <button className="get-started-btn">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
